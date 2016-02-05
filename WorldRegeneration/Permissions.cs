df93dad2e1fcb0983b62b1816d1f1db9adecc688
Marcus Koller <marcus101rr@gmail.com>
2016-02-05 18:10:47 -0500

    Added Permissions for World Regeneration

---
+++ WorldRegeneration/Permissions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WorldRegeneration
+{
+    public static class Permissions
+    {
+        [Description("Save a (.twd) terraria world data file.")]
+        public static readonly string saveworld = "worldregen.main.save";
+
+        [Description("Load a (.twd) terraria world data file.")]
+        public static readonly string loadworld = "worldregen.main.load";       
+
+    }
+}
