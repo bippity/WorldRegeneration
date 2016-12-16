55362e94db7ac10767f2ab00392a398af763a421
Marcus Koller <marcus101rr@gmail.com>
2016-12-16 13:34:40 -0500

    Updated API 2.0, Mintaka Update

--- WorldRegeneration/WorldRegeneration.cs
+++ WorldRegeneration/WorldRegeneration.cs
@@ -10,7 +10,7 @@ using TShockAPI.Hooks;
 
 namespace WorldRegeneration
 {
-    [ApiVersion(1, 25)]
+    [ApiVersion(2, 0)]
     public class WorldRegeneration : TerrariaPlugin
     {
         public override Version Version
